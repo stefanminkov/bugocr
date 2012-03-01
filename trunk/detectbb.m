79eff2eba1e66483512af2e1d04adada8fcdff82
nguyen.phuc.x <nguyen.phuc.x@4135dd95-e349-d9e9-87a8-bdb8f31f1012>
2012-03-01 08:00:08 +0000

    update new code

--- trunk/detectbb.m
+++ trunk/detectbb.m
@@ -1,18 +1,19 @@
-function guesses=detectbb(I,beta,settings)
+function heatmap=detectbb(I,beta,settings)
     maxout = settings.maxout;
     minout = settings.minout;
     
+    gray = rgb2gray(I);
+    heatmap = zeros(size(gray));
     bbs = slide_window(I);
     I = double(rgb2gray(I));
-    guesses = zeros(size(bbs,1),1);
     for i=1:size(bbs,1)
         i
+        bbsi = bbs(i,:);
         window = imcrop(I,bbs(i,:));
         features = fgen(window);
 
         features = normalize_feature(features',maxout(1:154),minout(1:154));
-        if p(features,beta) > .8
-            guesses(i) = 1;
-        end
+        prob = p(features,beta);
+        heatmap(bbsi(2):bbsi(2)+bbsi(4),bbsi(1):bbsi(1)+bbsi(3)) = heatmap(bbsi(2):bbsi(2)+bbsi(4),bbsi(1):bbsi(1)+bbsi(3)) + prob;
     end
 end
